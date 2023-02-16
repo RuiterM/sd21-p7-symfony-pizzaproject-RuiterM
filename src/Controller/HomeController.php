1f4a192ae8c3fcec51c2d6b95cb99e5d3b028b41
RuiterM <302078305@student.rocmondriaan.nl>
2023-02-16 15:29:39 +0100

    home pagina

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-
+    #[Route('/')]
     public function home(): Response
     {
         return $this->render('bezoeker/home.html.twig');
     }
 }
-?>
