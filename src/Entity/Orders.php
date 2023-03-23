a5639d47e9e9eb3464d3f5fafcc60fb387fb4a59
RuiterM <302078305@student.rocmondriaan.nl>
2023-03-23 15:31:42 +0100

    databse update

---
+++ src/Entity/Orders.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adres = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $inhoud = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAdres(): ?string
+    {
+        return $this->adres;
+    }
+
+    public function setAdres(string $adres): self
+    {
+        $this->adres = $adres;
+
+        return $this;
+    }
+
+    public function getInhoud(): ?string
+    {
+        return $this->inhoud;
+    }
+
+    public function setInhoud(string $inhoud): self
+    {
+        $this->inhoud = $inhoud;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
